9af0ed050b18b664f032b2fa41d28a32d8a42f20
KYu3RPI <97557061+KYu3RPI@users.noreply.github.com>
2024-02-24 15:43:58 -0500

    Basic web app with a component

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import styles from "./page.module.css";
+import Test from "@/components/test/test";
+
+export default function Page() {
+return(<div>
+    <Test />
+    </div>);
+}
---
+++ components/test/test.tsx
@@ -0,0 +1,6 @@
+export default function MyComponent() {
+    return (
+     <h1>This is my new Component</h1>
+    );
+  }
+  
