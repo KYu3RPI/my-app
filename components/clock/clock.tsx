47abfc35b16a4b044727923cc4d756955d1abccd
KYu3RPI <97557061+KYu3RPI@users.noreply.github.com>
2024-02-24 15:56:11 -0500

    Add style to component

--- components/clock/clock.tsx
+++ components/clock/clock.tsx
@@ -1,6 +1,8 @@
+import styles from './clock.module.css'
+
 export default function Clock() {
     return (
-  <div> 
+  <div className = {styles.clock}> 
   <h2> Current Date: {new Date().toLocaleDateString()} </h2>
   <h2> Current Time: {new Date().toLocaleTimeString()} </h2>
   </div>
